0120916cdf16d153053b376d1a4cda2c9527a700
Maksym Naumenko <maksymnaumenko2002@mail.ru>
2021-01-22 10:14:59 +0100

    major improvements
    added unminiefied version of the script, added BigInt support and more optimization.

--- README.md
+++ README.md
@@ -10,16 +10,27 @@ the constructor takes two arguments:
 
 ```js
 const b = new Base(16); // in this case the character range is from "0" to "f"
-b.encode(1744); // "6d0"
-b.parse("6d0"); // 1744
+b.Int.encode(1744); // "6d0"
+b.Int.parse("6d0"); // 1744
 ```
+
+## High precision encoding
+```js
+const b = new Base(36), // in this case the character range is from "0" to "z"
+      superBigNumber = 1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890n,
+      encoded = b.BigInt.encode(superBigNumber); // "6d0"
+
+b.BigInt.parse(encoded) === superBigNumber // true
+```
+
+**note that this function doesn't convert negative numbers to positive, so be careful. Also, the number passed as argument must be a BigInt!**
 
 ## Using custom sets of characters
 
 ```js
 const b = new Base(2,"%$"); // you can use any character
-b.encode(399); // returns "$$%%%$$$$", same as "110001111" in binary
-b.parse("$$%%%$$$$"); // 399
+b.Int.encode(399); // returns "$$%%%$$$$", same as "110001111" in binary
+b.Int.parse("$$%%%$$$$"); // 399
 ```
 *unless your base is higher than 128, you can omit the second argument (the script firstly uses a wide range of placeholder symbols)*
 
--- base.js
+++ base.js
@@ -1,1 +1,58 @@
-class Base{constructor(a,b){if(2>a)throw`Base cannot be less than 2!`;this.B=Math.floor(a);this._=(128<a?b:"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгдеёжзийклмнопрсиуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСИУФХЦЧШЩЪЫЬЗЮЯ").slice(0,a).split("")}parse(a){for(var b=0,c=0;c<a.length;c++)b+=this.B**(a.length-1-c)*this._.indexOf(a[c]);return b}encode(a){let t=this;a=Math.abs(a);for(var b="",c=t.e(a);-1<c;c--)for(let d=0;d<t._.length;d++){let e=t.g(c,d);if(e==a||t.g(c,d+1)>a){a-=e;b+=t._[d];break}}return b}g(a,b){return this.B**a*b}e(a){let b;for(b=0;200>b&&!(this.g(b+1,1)>a);b++);return b}}
+class Base {
+    constructor(a, b) {
+    	let t=this;
+        if (2 > a) throw `Base cannot be less than 2!`;
+        t.BASE = Math.floor(a);
+        t.BASE_CHARS = (typeof b != "undefined" || 128 < a ? b : "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгдеёжзийклмнопрсиуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСИУФХЦЧШЩЪЫЬЗЮЯ").slice(0, a).split("")
+    	
+    	let B = (n) => BigInt(B);
+        let getStepInt = (magnitude, step) => {return t.BASE ** magnitude * step}
+	    let estimateInt = (number) => {for (var magnitude = 0; 200 > magnitude && !(getStepInt(magnitude + 1, 1) > number); magnitude++);return magnitude}
+	    let getStepBigInt = (magnitude, step) => (B(t.BASE) ** B(magnitude) * B(step));
+        let estimateBigInt = (number) => {for (var magnitude = 0; 200 > magnitude && !(getStepBigInt(magnitude + 1, 1) > number); magnitude++);return magnitude}
+
+    	t.Int = {};
+    	t.BigInt = {};
+
+    	t.Int.parse = (int) => {
+        	for (var result = 0, digitIndex = 0; digitIndex < int.length; digitIndex++) result += t.BASE ** (int.length - 1 - digitIndex) * t.BASE_CHARS.indexOf(int[digitIndex]);
+        	return result;
+    	}
+
+    	t.Int.encode = (int) => {
+	       // let t = this;
+	        int = Math.abs(int);
+	        for (var result = "", magnitude = estimateInt(int); - 1 < magnitude; magnitude--)
+	            for (let step = 0; step < t.BASE_CHARS.length; step++) {
+	                let currentStep = getStepInt(magnitude, step);
+	                if (currentStep == int || getStepInt(magnitude, step + 1) > int) {
+	                    int -= currentStep;
+	                    result += t.BASE_CHARS[step];
+	                    break
+	                }
+	            }
+	        return result
+	    }
+	    t.BigInt.parse = (bigInt) => {
+	    	for (var result = B(0), digitIndex = B(0); digitIndex < bigInt.length; digitIndex++) result += B(t.BASE) ** (B(bigInt.length) - B(1) - digitIndex) * B(t.BASE_CHARS.indexOf(bigInt[digitIndex]));
+	        return result
+	    }
+	    t.BigInt.encode = (bigInt) => {
+	        for (var result = "", magnitude = estimateBigInt(bigInt); - 1 < magnitude; magnitude--)
+	            for (let step = 0; step < t.BASE_CHARS.length; step++) {
+	                let currentStep = getStepBigInt(magnitude, step);
+	                if (getStepBigInt(magnitude, step + 1) > bigInt || currentStep == bigInt) {
+	                    bigInt -= currentStep;
+	                    result += t.BASE_CHARS[step];
+	                    break
+	                }
+	            }
+	        return result
+	    }
+
+    }
+    
+} 
+
+let b = new Base(15);
+b.BigInt.encode(213161n);
